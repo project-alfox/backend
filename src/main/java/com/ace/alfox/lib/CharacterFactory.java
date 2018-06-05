7b4eb82f2164d2f9b060df34ca4e2e928baba7e6
Evan Verworn <evan@verworn.ca>
2018-06-05 00:52:06 -0400

    now 'persistent'

--- src/main/java/com/ace/alfox/lib/CharacterFactory.java
+++ src/main/java/com/ace/alfox/lib/CharacterFactory.java
@@ -3,13 +3,15 @@ package com.ace.alfox.lib;
 import com.ace.alfox.game.GameCharacter;
 
 public class CharacterFactory {
+    public static GameCharacter _testSubject = new GameCharacter();
 
     public static GameCharacter fetch(String id) {
         // TODO: make copy of character from database to prevent multi-thread access
-        return new GameCharacter();
+        return _testSubject;
     }
 
     public static void save(GameCharacter character) {
-        // TODO
+        // TODO: real implementation
+        _testSubject = character;
     }
 }
